143288b5ae988dc855cb85f3930b8ff86a786811
rupali-parab <rupalip.java@gmail.com>
2018-02-23 02:14:36 -0600

    verizon test code

--- robot_controller.js
+++ robot_controller.js
@@ -3,29 +3,21 @@
  * @author Rupali Parab <rupalip.java@gmail.com.com>
  */
 
+ (function() {
+
+     "use strict";
 
      var rbApp = angular.module('robots', []);
      rbApp.controller('RobotsController', ['$scope', function($scope) {
           $scope.robotType = ["Flying Robot", "Wheeled Robot", "Walking Robot"];
           $scope.robotColor = ["Gold", "Silver", "Bronze"];
           $scope.robotTable = [];
-            // $scope.showFirst = true;
-
-        //  $scope.robotTable.botType = [];
-        //  $scope.robotTable.botColor = [];
           $scope.buildMyBot = function(type, color){
 
             $scope.robotTable.push({
               botType: type,
               botColor: color
             });
-
-            // if($scope.robotTable){
-            //   $scope.showFirst = true;
-            // }
-            // if($scope.robotTable != null){
-            //     $scope.showFirst = false;
-            // }
 
           }
 
@@ -37,3 +29,4 @@
             }
           }
      }]);
+}());
